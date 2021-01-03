5a09898c2db1692ea74b1068ac3f6a7f48e3d38e
titikkoma88 <jie.koma88@gmail.com>
2021-01-03 14:04:05 +0700

    Hello World

--- public/index.php
+++ public/index.php
@@ -1,10 +1,12 @@
 <?php
 require _DIR_ .'/../vendor/autoload.php';
 
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
  
+
 use \LINE\LINEBot;
 use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use \LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
@@ -26,7 +28,7 @@ $app = AppFactory::create();
 $app->setBasePath("/public");
  
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello World!");
+    $response->getBody()->write("Hello World");
     return $response;
 });
  
